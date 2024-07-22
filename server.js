666262ca6067bf6a29a1349480e5bc7154e31d8f
Ryan Hood <ryanhood@ryans-mbp.lan>
2024-07-22 13:26:36 -0500

    finally fixed javascript functionality on first carousel

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files
+app.use(express.static(path.join(__dirname)));
+
+// Serve your index.html
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+
+// Define route for HowTo
+app.get('/HowTo', (req, res) => {
+  res.sendFile(path.join(__dirname, 'howto.html'));
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
