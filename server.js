06a423d02790b66b36ec2e59a75b9feb28e3d99c
Ryan Hood <ryanhood@ryans-mbp.lan>
2024-07-22 16:02:14 -0500

    fixed buttons on carousel

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const app = express();
 // Serve static files
 app.use(express.static(path.join(__dirname)));
 
+
 // Serve your index.html
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
